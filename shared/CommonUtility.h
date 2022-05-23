25e1d93637dda6180efcf4c47737448465214dc3
qbojj <105227351+qbojj@users.noreply.github.com>
2022-05-23 23:53:12 +0200

    fixes and faster

--- shared/CommonUtility.h
+++ shared/CommonUtility.h
@@ -7,16 +7,16 @@
 std::string GetFileString(const char *pth, bool binary);
 
 // akugb vakye must be POT
-template< typename V, typename A = decltype(alignof(std::max_align_t)) >
+template< typename V, typename A = decltype(alignof(max_align_t)) >
 std::enable_if_t< std::conjunction_v< std::is_integral< V >, std::is_integral< A > >, V >
 constexpr AlignOffset( V v, A a = alignof( max_align_t ) ) { return (-static_cast<std::make_signed_t<V>>(v)) & static_cast<V>(a - 1); };
 
 // align value must be POT
-template< typename V, typename A = decltype(alignof(std::max_align_t)) >
+template< typename V, typename A = decltype(alignof(max_align_t)) >
 std::enable_if_t< std::conjunction_v< std::is_integral< V >, std::is_integral< A > >, V >
 constexpr AlignVal( V v, A a = alignof( max_align_t) ) { return (v + static_cast<V>(a - 1)) & -static_cast<std::make_signed_t<V>>(a); };
 
-template< typename V, typename A = decltype(alignof(std::max_align_t)) >
+template< typename V, typename A = decltype(alignof(max_align_t)) >
 std::enable_if_t< std::conjunction_v< std::is_integral< V >, std::is_integral< A > >, V >
 constexpr AlignValNonPOT( V v, A a = alignof( max_align_t) ) { return (v + static_cast<V>(a - 1)) / static_cast<V>(a) * static_cast<V>(a); };
 
