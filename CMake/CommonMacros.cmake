8110cf1aea9deabb4e8ba79f9762181c16ee19e4
qbojj <kuba@ev1.pl>
2022-05-24 12:51:56 +0200

    cmanges in output directory of executables

--- CMake/CommonMacros.cmake
+++ CMake/CommonMacros.cmake
@@ -2,9 +2,9 @@ cmake_minimum_required(VERSION 3.12)
 
 macro(SETUP_COMMON projectname dirname )
 	# On Linux/macOS the binaries go to <root>/bin folder
-	if(UNIX)
-		set_target_properties(${projectname} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/bin")
-	elseif(MSVC)
+	#if(UNIX)
+	#	set_target_properties(${projectname} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_SOURCE_DIR}/bin")
+	if(MSVC)
 		set_property(TARGET ${projectname} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/data/")
 		#set_property(TARGET ${projectname} APPEND PROPERTY CXX_FLAGS "/Zc:__cplusplus /std:c17")
 	endif()
